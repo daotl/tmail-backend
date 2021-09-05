444fc3fa45eba623586edde8fca02f1a54c0739b
Nex Zhu <4370605+NexZhu@users.noreply.github.com>
2021-09-05 10:05:35 +0800

    fix: docker-compose scripts

--- tmail-backend/apps/distributed/init.sh
+++ tmail-backend/apps/distributed/init.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+
 VOLUMES_ROOT=/data/docker/volumes/tmail
 mkdir -p ${VOLUMES_ROOT}/elasticsearch/data
 mkdir -p ${VOLUMES_ROOT}/cassandra/data
 mkdir -p ${VOLUMES_ROOT}/rabbitmq/data
 mkdir -p ${VOLUMES_ROOT}/cloudserver/data
 mkdir -p ${VOLUMES_ROOT}/cloudserver/metadata
+
+docker network create --driver bridge emaily
--- tmail-backend/apps/distributed/up.sh
+++ tmail-backend/apps/distributed/up.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-docker network create --driver bridge emaily
 VOLUMES_ROOT=/data/docker/volumes/tmail \
 SCALITY_ACCESS_KEY_ID=MODIFY_ME \
 SCALITY_SECRET_ACCESS_KEY=MODIFY_ME \
